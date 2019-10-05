31a46397b2d583a441f860cefe5483a66a4eaee5
Rodriv01 <rodrigo.797.vargas@gmail.com>
2019-10-04 21:47:28 -0400

    Primer commit

---
+++ AdapterExample/AdapterExample/XmlConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AdapterExample
+{
+    public class XmlConverter
+    {
+        public XDocument GetXML()
+        {
+            ManufacturerDataProvider provider = new ManufacturerDataProvider();
+            var xDocument = new XDocument();
+            var xElement = new XElement("Manufacturers");
+            var xAttributes = provider.GetData()
+                .Select(m => new XElement("Manufacturer",
+                                    new XAttribute("City", m.City),
+                                    new XAttribute("Name", m.Name),
+                                    new XAttribute("Year", m.Year)));
+
+            xElement.Add(xAttributes);
+            xDocument.Add(xElement);
+
+            Console.WriteLine(xDocument);
+
+            return xDocument;
+        }
+    }
+}
